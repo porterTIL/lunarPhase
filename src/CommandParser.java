787028d046ba6fc46675b1edb048cbbf98f0c373
unknown <tristan.porter@tlgcohort.com>
2022-03-15 01:36:41 -0400

    add comment to regex to describe the purpose of its format

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -5,11 +5,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class CommandParser {
     private CommandParser(){}
+
+    // static Map actionWords = new HashMap(JsonImporter.parseDictionary());
+
+    // private static StringBuilder listValidItems(){
+    // StringBuilder actionTargetWords = new StringBuilder();
+    // for (String item : Inventory.items){
+    //    actionTargetWords.append(item);
+    //        }
+    //        for (String item : Map.currentLocation.items){
+    //                  actionTargetWords.append(item);
+    //        }
+    //        return actionTargetWords;
+    //    }
 
     static String parse(String request){
     //        listValidItems();
@@ -53,7 +65,7 @@ public class CommandParser {
             try (BufferedReader garbageFeed = new BufferedReader(new FileReader(garbageWordsFile))) {   // the BufferedReader wraps around the FileReader to make file reads more efficient
                 String garbageWord;
                 while ((garbageWord = garbageFeed.readLine()) != null) {
-                    request = request.replaceAll("\\b" + garbageWord.toUpperCase() + "\\b", "");
+                    request = request.replaceAll("\\b" + garbageWord.toUpperCase() + "\\b", ""); // the regex looks for the garbageWord with word delimeters on either side of it ex: preventing toga from removing to
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
